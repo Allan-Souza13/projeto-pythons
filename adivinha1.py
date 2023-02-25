f148f0c4298418100e63280810258ae89e0695ad
Allan Souza <vejeta300@hotmail.com>
2023-02-25 01:23:02 -0300

    projeto python

--- adivinha1.py
+++ adivinha1.py
@@ -4,21 +4,28 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
-chute_str  = input("Digite o seu numero: ")
+total_de_tentativas = 3
+rodada = 0
 
-print("Voce digitou ", chute_str)
+#enquanto ainda há tentativas:
+while (rodada < total_de_tentativas ):
+    print("Tentativas {} de {}".format( rodada, total_de_tentativas))#.format Strin Interpulation, substitui o {} de {}
+    chute_str  = input("Digite o seu numero: ")
+    print("Voce digitou ", chute_str)
+    chute = int(chute_str)#chute_str se transformou em int
 
-chute = int(chute_str)#chute_str se transformou em int
-acertou= chute == numero_secreto
-maior =  chute > numero_secreto
-menor =  chute < numero_secreto
+    acertou= chute == numero_secreto
+    maior =  chute > numero_secreto
+    menor =  chute < numero_secreto
 
-if (acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior do que o que o número secreto. ")
-    elif(menor):
-        print("Você errou! O seu chute foi menor do que o que o número secreto. ")
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o que o número secreto. ")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o que o número secreto. ")
+
+    rodada = rodada +1
 
 print("Fim do Jogo!")
---
+++ interpolation.py
@@ -0,0 +1,13 @@
+dia_ini = 24
+dia_fim = 28
+mes = "fevereiro"
+ano = 2017
+
+print("Em {} o carnaval acontece em {} do dia {} até dia {}.".format(ano, mes, dia_ini, dia_fim))
+
+dia = 28
+mes_Aniversario = "Outubro"
+ano_Aniversario = 2023
+idade = 24
+
+print("no dia {} de {} de {} irei comemorar os meus {} anos.".format(dia,mes_Aniversario,ano_Aniversario,idade))
