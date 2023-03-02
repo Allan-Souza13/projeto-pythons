2c4033a51b6095fff3a38bca9bfa2a10ecb90ed9
Allan Souza <vejeta300@hotmail.com>
2023-03-01 22:02:01 -0300

    python

--- formatar.py
+++ formatar.py
@@ -7,6 +7,19 @@ print("R$ {:7.2f}".format(1.59)) #deixara um espaço pois esta sendo informado 7
 
 print("R$ {:07.2f}".format(1.59)) #ira imprimir "0" no espaço antes do valor "1.59"
 
+print("R$ {:7.2f}".format(11.6))
+
+print("Meu nome é{1}{0}".format("Souza", "Allan"))#puxará o Allan primeiro
+
+idade = 14
+
+nome = "Allan Souza"
+mes = 12
+dia = 4
+ano = 1998
+
+print("Me chamo {}, a data do meu nacimento é: {}/{}/{}".format(nome,dia,mes,ano))
+
 #-----------------Inteiro------------------------#
 
 print("Data {:02d}/{:02d}".format(9,4))#irá imprimir o inteiro de data, usa-se "d" de digito
@@ -15,3 +28,4 @@ print("Data {:02d}/{:02d}".format(9,4))#irá imprimir o inteiro de data, usa-se 
 #Digamos que ele queira exibir a seguinte mensagem: "Ola Sr. Leonardo Cordeiro", como ele pode formatar a string para obter o resultado desejado?
 
 print("Olá Sr.{1} {0}".format("Cordeiro", "Leandro")) #A posição de Leandro é 1 e a de Cordeiro é 0.
+
