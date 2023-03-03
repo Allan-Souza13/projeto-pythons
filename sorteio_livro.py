943d19bd68e4a6314af587990e5ad7e2e9f91cf5
Allan Souza <vejeta300@hotmail.com>
2023-03-02 23:28:25 -0300

    sorteio

---
+++ sorteio_livro.py
@@ -0,0 +1,12 @@
+import random
+
+sorteado = random.randrange(0,4)
+
+print(sorteado)
+
+if sorteado == 1:
+    print("Paulo")
+elif sorteado == 2:
+    print("Juliana")
+else:
+    print("Tamires")
